6006e7655a15c144b763a978b75add977b845874
https://github.com/GaryLeeeee/Fellowship-activity.git <664306561@qq.com>
2018-03-13 09:21:13 +0800

    test0313

---
+++ src/main/java/com/garylee/weinan/WeinanApplication.java
@@ -0,0 +1,12 @@
+package com.garylee.weinan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeinanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeinanApplication.class, args);
+	}
+}
